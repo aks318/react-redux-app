a74e032b3fd5c941e95c615b87824f04a13ac9da
aakash-1105 <aakash@inviziosolutions.com>
2022-04-13 11:49:12 +0530

    action payload added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CakeContainer from './Components/CakeContainer';
 import store from './redux/store';
 import HooksCakeContainer from './Components/HooksCakeContainer';
 import IceCreamContainer from './Components/IceCreamContainer';
+import NewCakeContainer from './Components/NewCakeContainer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <CakeContainer />
       <HooksCakeContainer />
       <IceCreamContainer />
+      <NewCakeContainer />
     </div>
     </Provider>
   );
